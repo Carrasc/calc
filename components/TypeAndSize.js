8ab85833f8988b3082e72241e6b5ea8dc411a67c
carrasc <luiscbilbao@gmail.com>
2022-09-19 14:49:10 -0500

    Styles for Inputs and texts

--- components/TypeAndSize.js
+++ components/TypeAndSize.js
@@ -2,67 +2,107 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import MenuItem from "@mui/material/MenuItem";
+import { alpha, styled } from "@mui/material/styles";
 
 const typeOfProyects = [
   {
-    value: "Obra nueva",
+    value: 1,
     label: "Obra nueva",
   },
   {
-    value: "Ampliación",
+    value: 1.2,
     label: "Ampliación",
   },
   {
-    value: "Reacondicionamiento",
+    value: 1.25,
     label: "Reacondicionamiento",
   },
   {
-    value: "Rehabilitación",
+    value: 1.3,
     label: "Rehabilitación",
   },
   {
-    value: "Remodelación",
+    value: 1.5,
     label: "Remodelación",
   },
 ];
 
+const CustomTextField = styled(TextField)({
+  "& .MuiInputBase-input": {
+    fontFamily: "Montserrat",
+    textAlign: "center",
+  },
+  "& label.Mui-focused": {
+    color: "green",
+  },
+  "& .MuiInput-underline:after": {
+    borderBottomColor: "#7AC279",
+  },
+  "& .MuiOutlinedInput-root": {
+    "& fieldset": {
+      borderColor: "gray",
+      borderRadius: "6px",
+      borderWidth: "1px",
+      color: "green",
+    },
+    "&.Mui-focused fieldset": {
+      borderColor: "#7AC279",
+    },
+  },
+});
+
 export default function TypeAndSize() {
   const [typeOfProyect, setCurrency] = React.useState("EUR");
 
-  const handleChange = (event) => {
+  const handleChangeType = (event) => {
     setCurrency(event.target.value);
+    console.log(event.target.value);
+  };
+
+  const handleChangeSize = (event) => {
+    console.log(event.target.value);
   };
 
   return (
-    <Box
-      component="form"
-      sx={{
-        "& .MuiTextField-root": { m: 1, width: "25ch" },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <div>
-        <div className="my-6">
-          <p className="text-center">¿Cual es la modalidad del proyecto?</p>
-          <TextField
-            id="outlined-select-currency"
+    <Box component="form" noValidate autoComplete="off">
+      <div class="grid grid-cols-1 gap-8">
+        <div class="grid justify-items-center">
+          <p class="text-center mb-4 font-[Montserrat-bold] text-sm">
+            ¿Cual es la modalidad del proyecto?
+          </p>
+          <CustomTextField
+            id="typeProject"
             select
             label=""
             value={typeOfProyect}
-            onChange={handleChange}
+            onChange={handleChangeType}
+            sx={{ width: 200, minWidth: 100 }}
+            size="small"
           >
             {typeOfProyects.map((option) => (
-              <MenuItem key={option.value} value={option.value}>
+              <MenuItem
+                key={option.value}
+                value={option.value}
+                style={{ fontSize: 14, fontFamily: "Montserrat" }}
+              >
                 {option.label}
               </MenuItem>
             ))}
-          </TextField>
+          </CustomTextField>
         </div>
 
-        <div>
-          <p className="text-center">¿Cuantos metros cuadrados?</p>
-          <TextField id="outlined-number" label="" type="number" />
+        <div class="grid justify-items-center">
+          <p class="text-center mb-4 font-[Montserrat-bold] text-sm">
+            ¿Cuantos metros cuadrados?
+          </p>
+          <CustomTextField
+            onChange={handleChangeSize}
+            id="sizeOfProject"
+            label=""
+            type="number"
+            sx={{ width: 200, minWidth: 100 }}
+            size="small"
+          />
         </div>
       </div>
     </Box>
