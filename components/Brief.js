b56a7e61c2cb3dc22543ed0f97dd235ad84a5a13
maupeon <maupeon@gmail.com>
2022-09-20 23:10:32 -0500

    Update Brief.js

--- components/Brief.js
+++ components/Brief.js
@@ -4,6 +4,20 @@ export default function Brief() {
 	return (
 		<div className="flex flex-col items-center justify-center w-full my-20">
 			<p className="mb-5 ">¿Están todos los datos en orden?</p>
+			<div className="flex items-center w-full justify-evenly ">
+				<div>Hola</div>
+				<div className="w-full mx-10 h-[1px] bg-green-500 "></div>
+				<div className=" font-montserrat">caca</div>
+			</div>
+			<div className="flex items-center w-full mt-5 break-all justify-evenly ">
+				<div className="">
+					aksd ñlfjñsdljfñ f asd ff asdmnfkjsadlfkjlsdjflsdjkdf
+				</div>
+				<div className="w-full mx-10 h-[1px] bg-green-500 "></div>
+				<div className=" font-montserrat">
+					csd kfjalsdjfñl kasdjfñlkj asñlaca
+				</div>
+			</div>
 		</div>
 	);
 }
