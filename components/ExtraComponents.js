736c1620da6c4a1b7352f6d1f3c273a98e0aed60
carrasc <luiscbilbao@gmail.com>
2022-09-20 23:08:32 -0500

    Extra components

---
+++ components/ExtraComponents.js
@@ -0,0 +1,65 @@
+import * as React from "react";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import extraInstalation from "../Data/IC.json";
+
+const extraComponents = [
+  {
+    value: "AA",
+    label: "Aire Acondicionado",
+  },
+  {
+    value: "VE",
+    label: "Ventilacion y Extraccion",
+  },
+  {
+    value: "GM",
+    label: "Gases Medicinales",
+  },
+  {
+    value: "PCI",
+    label: "Proteccion Contra Incendios (PCI)",
+  },
+  {
+    value: "SCA",
+    label: "Sistema de Control de accesos",
+  },
+  {
+    value: "SVD",
+    label: "Sistema de Voz y Datos",
+  },
+  {
+    value: "CCTV",
+    label: "Sistema de CCTV",
+  },
+];
+
+export default function ExtraComponents() {
+  return (
+    <div>
+      <p className="text-center mb-6 font-[Montserrat-bold] text-sm ">
+        ¿Que alcances extras se tienen en consideracion?
+      </p>
+      <div className="grid grid-cols-4">
+        {extraInstalation.map((item, index) => (
+          <div className="flex justify-items-end">
+            <FormControlLabel
+              label={item.label}
+              control={
+                <Checkbox
+                  sx={{
+                    color: "#707070",
+                    "&.Mui-checked": {
+                      color: "#7AC279",
+                    },
+                  }}
+                />
+              }
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,35 @@
-import { useTheme } from 'next-themes';
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
-import { MoonIcon, SunIcon } from '@heroicons/react/24/outline';
-import ArcStepper from '../components/ArcStepper';
-import TypeAndSize from '../components/TypeAndSize';
-import TypeOfProyects from '../components/TypeOfProyects';
-import { steps } from '../Data/Steps';
-import { useContext, useState } from 'react';
-import { GeneralContext } from '../Context/GeneralContext';
-import EstadosYMunicipios from '../components/EstadosYMunicipios';
-import ProyectPlans from '../components/ProyectPlans';
-import TimesOfProyect from '../components/TimesOfProyect';
+import { useTheme } from "next-themes";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { MoonIcon, SunIcon } from "@heroicons/react/24/outline";
+import ArcStepper from "../components/ArcStepper";
+import TypeAndSize from "../components/TypeAndSize";
+import TypeOfProyects from "../components/TypeOfProyects";
+import { steps } from "../Data/Steps";
+import { useContext, useState } from "react";
+import { GeneralContext } from "../Context/GeneralContext";
+import EstadosYMunicipios from "../components/EstadosYMunicipios";
+import ProyectPlans from "../components/ProyectPlans";
+import TimesOfProyect from "../components/TimesOfProyect";
+import ExtraComponents from "../components/ExtraComponents";
 export default function Home() {
-	const { theme, setTheme } = useTheme();
-	const [activeStep, setActiveStep] = useState(0);
-	const [selected, setSelected] = useState(undefined);
-	const { generalValue } = useContext(GeneralContext);
+  const { theme, setTheme } = useTheme();
+  const [activeStep, setActiveStep] = useState(0);
+  const [selected, setSelected] = useState(undefined);
+  const { generalValue } = useContext(GeneralContext);
 
-	const handleNext = () => {
-		setActiveStep((index) => index + 1);
-	};
-	const handlePrev = () => {
-		setActiveStep((index) => index - 1);
-	};
+  const handleNext = () => {
+    setActiveStep((index) => index + 1);
+  };
+  const handlePrev = () => {
+    setActiveStep((index) => index - 1);
+  };
 
-	console.log({ generalValue });
-	return (
-		<div className=" w-full py-20 max-w-6xl mx-auto px-10  font-[Montserrat-bold]">
-			{/* <button
+  console.log({ generalValue });
+  return (
+    <div className=" w-full py-20 max-w-6xl mx-auto px-10  font-[Montserrat-bold]">
+      {/* <button
 				className="absolute p-2 border-2 rounded-xl top-10 border-neutral-200 dark:border-neutral-600 right-10 dark:bg-neutral-600 bg-neutral-200 hover:border-2 hover:border-indigo-400 dark:hover:border-indigo-400"
 				onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
 			>
@@ -38,21 +39,22 @@ export default function Home() {
 					<MoonIcon className="w-5 h-5" />
 				)}
 			</button> */}
-			<TypeOfProyects />
-			<TypeAndSize />
-			<EstadosYMunicipios />
-			<ProyectPlans />
-			<TimesOfProyect />
-			{/* <div className="flex flex-col items-center justify-center w-full h-full max-w-6xl px-5 mx-auto overflow-y-scroll md:px-20"> */}
-			{/* {steps[activeStep].component({ handleNext: handleNext })} */}
-			{/* </div> */}
-			{/* <div className=" h-[10vh] w-full flex justify-center items-center">
+      <TypeOfProyects />
+      <TypeAndSize />
+      <EstadosYMunicipios />
+      <ProyectPlans />
+      <ExtraComponents />
+      <TimesOfProyect />
+      {/* <div className="flex flex-col items-center justify-center w-full h-full max-w-6xl px-5 mx-auto overflow-y-scroll md:px-20"> */}
+      {/* {steps[activeStep].component({ handleNext: handleNext })} */}
+      {/* </div> */}
+      {/* <div className=" h-[10vh] w-full flex justify-center items-center">
 				<ArcStepper
 					handleNext={handleNext}
 					handlePrev={handlePrev}
 					activeStep={activeStep}
 				/>
 			</div> */}
-		</div>
-	);
+    </div>
+  );
 }
